aaacc4cb2f3250325e2131d1ba79b5cf46bf31a1
VaishantMakan <vaishantmakan@gmail.com>
2023-12-09 21:46:38 +0530

    easy-expenditure-analysis-solved

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,39 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  //create an object that will store the different categories and their total price in all transactions.
+
+  let allCategories = {};
+
+  for (let i = 0; i < transactions.length; i++) {
+    //first get the transaction
+    const transaction = transactions[i];
+
+    let category = transaction.category;
+    let price = transaction.price;
+
+    //if the category already exists in the allCategories obj, then add the price, else add the category
+    if (allCategories.hasOwnProperty(category)) {
+      allCategories[category] += price;
+    } else {
+      allCategories[category] = price;
+    }
+  }
+
+  //now creating an array that will store objs, with the properties category and price.
+
+  let result = [];
+
+  for (const category in allCategories) {
+    let finalObj = {
+      category: category,
+      totalSpent: allCategories[category],
+    };
+
+    result.push(finalObj);
+  }
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
